54bce96fb74b80868c5c3fe63692660217af9c02
Ivo Lopes <ivolopes_17@hotmail.com>
2020-01-24 09:09:59 -0300

    api-gateway, config-server and service-discovery

---
+++ config-server/src/test/java/br/com/ifood/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifood.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
